9b20ac98057279057258b9f9401b0258090e29d2
Maxwell Twente <maxtwente@gmail.com>
2018-08-15 16:25:24 -0500

    ASP.NET MVC social networking website
    ASP.NET MVC social networking website

---
+++ NotFaceBook/Models/Classes/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NotFaceBook.Models.Classes
+{
+    public class Post
+    {
+        public int postId { get; set; }
+        public string postText { get; set; }
+        public DateTime postDateTime { get; set; }
+        public int postLike { get; set; }
+    }
+}
